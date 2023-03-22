56cc513ba25d664f918e56f006fe1bdf007172f2
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-03-22 17:54:43 +0300

    JDBC Intro

---
+++ src/test/java/TestConnection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        String url ="jdbc:oracle:thin:@54.236.150.168:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection was successful ");
+        } catch (SQLException e) {
+            System.out.println("Connection has failed "+ e.getMessage());
+        }
+
+
+    }
+}
