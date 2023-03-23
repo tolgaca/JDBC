23457f90fb305293e0efd3585970c3b6f42a9b91
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-03-23 17:35:21 +0300

    Looping Result Set while loop

--- src/test/java/LoopingResultSet.java
+++ src/test/java/LoopingResultSet.java
@@ -1,3 +1,31 @@
+import java.sql.*;
+
 public class LoopingResultSet {
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM REGIONS");
+
+            while ((rs.next())){
+                System.out.println(rs.getString(1) + " | " + rs.getString(2));
+            }
+
+            rs.beforeFirst();
+            while ((rs.next())){
+                System.out.println(rs.getString(1) + " | " + rs.getString(2));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error Has Occurred " + e.getMessage());
+        }
+
+    }
 
 }
