127f2db9798082da842a1e39b3701d7a1d3c0820
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-03-23 18:02:02 +0300

    Getting all rows and columns with Result Set Meta Data

--- src/test/java/ResultSetMetaData_ForColumnInfo.java
+++ src/test/java/ResultSetMetaData_ForColumnInfo.java
@@ -13,17 +13,30 @@ public class ResultSetMetaData_ForColumnInfo {
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM EMPLOYEES");
 
-            ResultSetMetaData rsmd =rs.getMetaData();
+            ResultSetMetaData rsmd =rs.getMetaData(); //YOU CAN COLUMN RELATED INFO IN THIS WAY
             int columnCount = rsmd.getColumnCount();
-            String firstColumn = rsmd.getColumnName(1);
+            //String firstColumn = rsmd.getColumnName(1);
 
-            System.out.println("columnCount = " + columnCount);
-            System.out.println("firstColumn = " + firstColumn);
+            //System.out.println("columnCount = " + columnCount);
+            //System.out.println("firstColumn = " + firstColumn);
 
-            for (int colIndex = 1; colIndex < columnCount; colIndex++) {
+            for (int colIndex = 1; colIndex <= columnCount; colIndex++) {
 
-                System.out.println("rsmd.getColumnName("+colIndex+") = "+ rsmd.getColumnName(colIndex));
+                System.out.print(rsmd.getColumnName(colIndex)+" \t");
+
             }
+            System.out.println();
+            while (rs.next()){
+                //printing all data in one row
+                rs.next();
+                for (int colIndex = 1; colIndex <= columnCount; colIndex++) {
+                    System.out.print(rs.getString(colIndex)+ " \t");
+
+                }
+                System.out.println();
+            }
+
+
 
         } catch (SQLException e) {
             System.out.println("Error Has Occurred " + e.getMessage());
