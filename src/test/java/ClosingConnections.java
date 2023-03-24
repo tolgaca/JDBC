92aefd1b131a335a2a8aa236d7c37c5aaad2a5cd
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-03-24 14:54:07 +0300

    Closing Resources Intro

--- src/test/java/ClosingConnections.java
+++ src/test/java/ClosingConnections.java
@@ -1,2 +1,28 @@
+import java.sql.*;
+
 public class ClosingConnections {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs = stmt.executeQuery("SELECT * FROM EMPLOYEES");
+
+
+
+
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+    }
+
+
+
+
 }
