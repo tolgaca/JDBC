71405d567c1ac6495705cb0e67b26f2b2a46a0cb
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-03-27 14:59:54 +0300

    DB_Utility_Recording

--- src/test/java/DB_Utility_Recording.java
+++ src/test/java/DB_Utility_Recording.java
@@ -2,13 +2,17 @@ import java.sql.*;
 
 public class DB_Utility_Recording {
 
+    static Connection conn;
+    static Statement stmt;
+    static ResultSet rs;
+
     public  static  void createConnection(){
         String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE";
         String username = "hr";
         String password = "hr";
 
         try {
-            Connection conn = DriverManager.getConnection(url, username, password);
+            conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connection successful");
 
         }catch (SQLException e){
@@ -16,11 +20,53 @@ public class DB_Utility_Recording {
         }
     }
 
+    public static ResultSet runQuery(String query){
+
+        try {
+             stmt= conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+             rs = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            System.out.println("ERROR WHILE GETTING RESULTSET");
+        }
+        return rs;
+    }
 
 
-    public static void main(String[] args) {
+    public static  void destroy(){
 
 
+            try {
+                if(rs!= null) {
+                    rs.close();
+                }
+                if(stmt!= null) {
+                    stmt.close();
+                }
+                if(conn!= null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+    }
+
+    public  static int getRowCount(){
+
+        int rowCount = 0;
+        rs.last();
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+        createConnection();
+        ResultSet myResult = runQuery("SELECT * FROM REGIONS");
+        rs.next();
+        System.out.println(rs.getString(1));
+
+        destroy();
 
     }
 }
